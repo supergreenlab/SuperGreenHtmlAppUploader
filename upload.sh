c1563705230e997b67e1620c66cc23a76d7fee33
vitaminwater <stantin04@gmail.com>
2019-09-20 16:26:35 +0200

    better script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+set -e
+
+GREEN="\033[0;32m"
+NC="\033[0m"
+
+echo -e "Creating spiffs_fs_gz files:"
+mkdir -p spiffs_fs_gz
+rm -rf spiffs_fs_gz/*
+for i in $(ls ./spiffs_fs); do
+  gzip -c "./spiffs_fs/$i" >> "./spiffs_fs_gz/$i"
+  echo -e "Created spiffs_fs_gz/$i: ${GREEN}Done${NC}"
+done
+
 curl -XPOST --data-binary @spiffs_fs_gz/config.json http://$1/fs/config.json
 curl -XPOST --data-binary @spiffs_fs_gz/app.html http://$1/fs/app.html
+
+rm -rf spiffs_fs_gz
